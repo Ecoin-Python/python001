b734fe2a141fe56a23913284e19b3641d1a9ed4e
Abdelouahab Djoudi <djoudi@AppsBunches.com>
2023-06-07 22:13:32 +0100

    add varialbr and type var

--- app.py
+++ app.py
@@ -1,2 +1,61 @@
-print("Welcom to ecoin")
-x = 25
+#  print("Ecoin2023")
+# VAR - TYPE VAR - FUNCTION sTRING  LIST  EXCERCIE  - CONDITION LOOP
+
+C = "Ecoin"  # str
+C = 25   # int
+C = 25.36
+# Dynamic Typing
+
+#azAZ09_
+# ecoin = 36
+
+# ec36 = "Ecoin"
+# _Ecoin = 85
+# a = 15
+# print(a)
+# Int 36  float 16.36   str 'ecoin'  "Ecoin" Boolean Bool True  False
+
+# x = 48 
+# print(type(x))
+# note = 14.25 
+# print(type(note))
+# lang = 'Rust' 
+# print(type(lang))
+
+# isAdmin = False
+# print(type(isAdmin))
+
+# String
+# l = len(lang)
+
+# print(l)
+#l = lang.upper()
+#print(l)
+
+# l = lang.lower()
+# print(l)
+#Concat
+# print("Welcom to Language :"+lang)
+# v = 88
+#v = 36.88
+#v = True
+
+#print("Welcom to year :"+v)
+
+""" str = "ecoin Welcom ecoin to ecoin"
+c = str.count("ecoin")
+print(c) """
+#print(str)
+#str = str.replace("co","Z")
+
+#print(str)
+
+p = 25 
+print(type(p))
+
+
+
+
+
+
+   
