7b62f54d2ea489c7afe464333d8eb5545f926088
Abdelouahab Djoudi <djoudi@AppsBunches.com>
2023-06-15 21:23:07 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -13,9 +13,30 @@ str = "Welcom to Ecoin"
     print(s) """
     
     
-for i in 100,200,300:
+""" for i in 100,200,300:
     print(i * 2)
+ """
+ 
+""" for i in range(1, 11):
+    print(i) """
+ 
+""" for i in range(1, 11):
+    print(i ** 2) """
+ 
+""" x = 25
+#x = x + 10
+x += 10
+x = x - 5 """
 
+#print(x)
+sum_even  = 0
+#2 + 4 + 6 + 8 + 10 ....20
+#0 + 2 + 4 + 6
+""" for i in range(2, 21, 2):
+    sum_even += i
+    
+print("Sum of even numbers : ", sum_even) """
+ 
 
 
 
