d0ae9fdbddc563485d80472b0f146475ab449417
Abdelouahab Djoudi <djoudi@AppsBunches.com>
2023-06-21 22:21:16 +0100

    update

--- mystring.py
+++ mystring.py
@@ -16,8 +16,20 @@ str = 'Ecoin'
 
 #print('o' in str )
 
-newlist = [nl for nl in lang_tuple if 'i' in nl]
-print(newlist)
+""" newlist = [nl for nl in lang_tuple if 'i' in nl]
+print(newlist) """
+l1 = [20,40,6,50]
+l2 = [2,4,6,9]
+mylist = [] # 40 80 120 80 160
+for x in l1:  #[20,40,60]
+    for y in l2:  #[2,4,6]
+        mylist.append(x * y)
+        
+print(mylist)
+
+# by list comprehension
+mylist2 = [x * y for x in l1 for y in l2]
+print(mylist2)
 
 
 
